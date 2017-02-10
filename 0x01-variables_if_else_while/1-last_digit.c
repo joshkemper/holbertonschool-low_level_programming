73c67f5837ae306e8d0dbb03c858fea1d7a0354d
Chris Turner <clturner23@gmail.com>
2017-02-10 02:06:51 +0000

    remove space above loop

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,6 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	lastDigit = n % 10;
-
 	if (lastDigit == 0)
 	{
 printf("Last digit of %d is %d and is 0", n, lastDigit);
@@ -26,10 +25,6 @@ printf("Last digit of %d is %d and is less than 6 and not 0", n, lastDigit);
 	else if (lastDigit >= 6)
 	{
 printf("Last digit of %d is %d and is greater than 5", n, lastDigit);
-	}
-	else
-	{
-printf("%d whoooah", lastDigit);
 	}
 	putchar ('\n');
 	return (0);
