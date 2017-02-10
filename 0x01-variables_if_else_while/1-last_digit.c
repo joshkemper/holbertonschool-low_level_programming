cdbe0d0daa55c03cfe2aac16a85b7655b38f07ee
Chris Turner <clturner23@gmail.com>
2017-02-10 01:59:22 +0000

    rmove \n's from prinf's

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,25 +10,27 @@ int main(void)
 {
 	int n;
 	int lastDigit;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastDigit =n%10;
+	lastDigit = n % 10;
 
 	if (lastDigit == 0)
 	{
-  printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+printf("Last digit of %d is %d and is 0", n, lastDigit);
 	}
 	else if (lastDigit <= 5)
 	{
-  printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+printf("Last digit of %d is %d and is less than 6 and not 0", n, lastDigit);
 	}
 	else if (lastDigit >= 6)
 	{
-  printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+printf("Last digit of %d is %d and is greater than 5", n, lastDigit);
 	}
 	else
 	{
-  printf("%d whoooah", lastDigit);
+printf("%d whoooah", lastDigit);
 	}
+	putchar ('\n');
 	return (0);
 }
