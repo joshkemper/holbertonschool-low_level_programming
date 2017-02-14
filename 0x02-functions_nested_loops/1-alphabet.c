bde2477c22fbd12b8be6209c269ce00ebe42964f
Chris Turner <clturner23@gmail.com>
2017-02-14 21:48:29 +0000

    add comments and function name

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
 #include "holberton.h"
 /**
- * main - Entry point
+ * print_alphabet - prits alphabet a - z lower case
  *
- * Return: Always 0 (
+ * Return: Always 0
  */
 void print_alphabet(void)
 {
 	int i;
 
-	for(i = 'a'; i <= 'z'; i++)
+	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
-        }
+	}
 	_putchar('\n');
 }
