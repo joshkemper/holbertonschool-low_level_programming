8ddac0355aa8153ca53560617656279d79bb4f48
Chris Turner <clturner23@gmail.com>
2017-02-14 21:41:10 +0000

    change code to all putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,14 +6,15 @@
  */
 int main(void)
 {
-	char c[] = "Holberton";
-	int i = 0;
-
-	while (c[i] != '\0')
-	{
-		_putchar(c[i]);
-		i++;
-	}
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 	return (0);
 }
