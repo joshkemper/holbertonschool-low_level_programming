9c824a4ee45f4e87ccb77881acbc707051308cbf
Chris Turner <clturner23@gmail.com>
2017-02-15 04:10:49 +0000

    prints every minute of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+* jack_bauer - prints every minute of the day of Jack Bauer, 00:00 to 23:59
+*
+*
+* Description -prints every minute of the day of Jack Bauer,00:00 to 23:59
+*
+* Return: void function
+*/
+void jack_bauer(void)
+{
+	int a, b = 0;
+
+	while (a <= 23)
+	{
+		while (b <= 59)
+		{
+			_putchar(a / 10 + '0');
+			_putchar(a % 10 + '0');
+			_putchar(':');
+			_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
+			_putchar('\n');
+			b++;
+		}
+		b = 0;
+		a++;
+	}
+}
