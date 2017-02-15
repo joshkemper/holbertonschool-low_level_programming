00c1abdb53a7c30b0656c49a839222bd147b5579
Chris Turner <clturner23@gmail.com>
2017-02-15 21:55:36 +0000

    times table to 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
  * times_table - times tanble to 9
  *
@@ -12,32 +11,30 @@ void times_table(void)
 {
 	int i;
 	int j;
+	int k;
 
-	_putchar (' ');
-	_putchar (' ');
-	for (i = 0; i < 10; ++i)
+	for (j = 0; j < 10; ++j)
 	{
-
 		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-
-	}
-	for (i = 1; i < 64; ++i)
-		_putchar(' ');
-	_putchar('\n');
-
-	for (i = 1; i < 10; ++i)
-	{
-		_putchar(' ');
-		_putchar(' ');
-		_putchar('0');
-		_putchar(',');
-
-		for (j = 1; j < 10; ++j)
-			printf("%3d,", i * j);
-		printf("\n");
+		for (i = 1; i < 10; i++)
+		{
+			k = i * j;
+			if (k > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(k / 10 + '0');
+				_putchar(k % 10 + '0');
+			}
+			else
+			{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(k + '0');
+			}
+		}
+		_putchar('\n');
 	}
 
 }
