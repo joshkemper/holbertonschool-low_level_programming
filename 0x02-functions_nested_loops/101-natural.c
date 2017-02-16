28622113feaf2b0b02976ed914ee31808279816d
Chris Turner <clturner23@gmail.com>
2017-02-16 01:44:20 +0000

    Write a program that computes and prints the sum of all the multiples of 3 or 5 below 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: print sum of all digits divisble by 3,5 through 1024
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int count;
+	int div3;
+	int div5;
+	int sum;
+
+	sum = 0;
+	for (count = 0; count < 1024; count++)
+	{
+		div3 = count % 3;
+		div5 = count % 5;
+		if(div3 == 0 || div5 == 0)
+		{
+			sum += count;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
