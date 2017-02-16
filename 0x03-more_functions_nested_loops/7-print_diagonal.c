b4f4a27c4231261d622924b0be7ef57db24884fa
Chris Turner <clturner23@gmail.com>
2017-02-16 23:26:54 +0000

    function that draws a diagonal line

---
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * print_diagonal - print diagonal
+ * @n: integer set size of triangle
+ * Description: prints diagonal line of slashes
+ *
+ * Return: 0
+ */
+void print_diagonal(int n)
+{
+	int j;
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j <= i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
