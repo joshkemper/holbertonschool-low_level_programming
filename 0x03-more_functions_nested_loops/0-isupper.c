ce5ba1cb6c972947cab426635afbc3fd96e5b921
Chris Turner <clturner23@gmail.com>
2017-02-16 16:41:36 +0000

    checks for upper case letters

---
+++ 0x03-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * Main - entry point
+ * @c: varibale to test if uppcase
+ * Description - checks to see if a letter is uppcase (1) or not (0)
+ *
+ *Return: 1 for uppcase 0 for all other
+ */
+int _isupper(int c)
+{
+	if (c <= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
