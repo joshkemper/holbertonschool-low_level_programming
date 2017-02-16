8745b99e5db7d3c992e10c15fb24d7d4b0259936
Chris Turner <clturner23@gmail.com>
2017-02-16 23:38:56 +0000

    change comment

--- 0x03-more_functions_nested_loops/0-isupper.c
+++ 0x03-more_functions_nested_loops/0-isupper.c
@@ -5,7 +5,7 @@
  * @c: varibale to test if uppcase
  * Description - checks to see if a letter is uppcase (1) or not (0)
  *
- *Return: 1 for uppcase 0 for all other
+ * Return: 1 for uppcase 0 for all other
  */
 int _isupper(int c)
 {
