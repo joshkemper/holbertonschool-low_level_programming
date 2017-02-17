aa66bb385839eeda6056a7b42dd98c7c70976bb9
Chris Turner <clturner23@gmail.com>
2017-02-17 01:41:39 +0000

    remove whitespace

--- 0x03-more_functions_nested_loops/7-print_diagonal.c
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -1,10 +1,12 @@
 #include "holberton.h"
 /**
- * print_diagonal - print diagonal /s size of n
- * @n: integer set size of triangle
- * Description: prints diagonal line of slashes
- * Return: Void
- */
+ * print_diagonal - ptints slashes in diagonal line n times
+ * @n: times to print slash
+(*
+* Description: prints slashes in diagonal line to size n
+(*
+* Return: Void
+*/
 void print_diagonal(int n)
 {
 	int j;
@@ -18,7 +20,7 @@ void print_diagonal(int n)
 	{
 		for (i = 1; i <= n; i++)
 		{
-			for (j = 1; j <= i -1; j++)
+			for (j = 1; j <= i - 1; j++)
 			{
 				if (j == 0 && i == 0)
 				{
