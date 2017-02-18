6062f922ae42bc6ec394020a82e307318635edf4
Chris Turner <clturner23@gmail.com>
2017-02-18 02:08:38 +0000

    code to find highest prime of a number

---
+++ 0x03-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: finds largest prime factor of 612852475143
+ *
+ * Return: Void
+ */
+int main(void)
+{
+	long  number = 612852475143;
+	long div = 2;
+	long maxFact;
+
+	while (number != 0)
+	{
+		if (number % div != 0)
+			div = div + 1;
+		else
+		{
+			maxFact  = number;
+			number = number / div;
+			if (number == 1)
+			{
+				printf("%ld", maxFact);
+				break;
+			}
+		}
+	}
+}
