3e4bd48368e9b4dde5deb86f4dd1b248d00f0185
Chris Turner <clturner23@gmail.com>
2017-02-21 02:38:10 +0000

    takes a pointer and changes value it points to

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * reset_to_98 - takes a pointer to an int as parameter update value to 98
+ * @n: integer whos value is changed in function
+ * Description: takes a pointer changes value to 98
+ *
+ * Return: return n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
