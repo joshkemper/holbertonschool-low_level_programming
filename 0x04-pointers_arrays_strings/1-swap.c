2c932d7553eba9826baa619dd6b1a49889134aa1
Chris Turner <clturner23@gmail.com>
2017-02-21 16:12:43 +0000

    switches values of two pointers

---
+++ 0x04-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * swap_int - swap intger values of a sand b pointers
+ * @a: first integer pointer
+ * @b: second integer pointer
+ * Description: swap values of pointer a and b
+ *
+ * Return:
+ */
+void swap_int(int *a, int *b)
+{
+	int i;
+
+	i = *a;
+	*a = *b;
+	*b = i;
+	return (0);
+}
