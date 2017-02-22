c06ac15f56e04f0aae1499f20569874a1a9d1bc8
Chris Turner <clturner23@gmail.com>
2017-02-22 02:02:09 +0000

    put end of string

---
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ * puts_half - putslast half of string for even number strings and -1 /2 odd
+ * @str: pointer to string
+ * Description: putslast half of a string
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int length = 0;
+	int count = 0;
+	int i;
+
+	while (str[length] != 0)
+	{
+		length++;
+		i = length++;
+	}
+	if (length % 2 != 0)
+	{
+		_putchar((length -1) /2);
+	}
+	else
+	{
+	length = (length / 2);
+	}
+        for (count = 0; count <= i; count++)
+	{
+		_putchar(str[length]);
+		length++;
+	}
+	_putchar('\n');
+}
