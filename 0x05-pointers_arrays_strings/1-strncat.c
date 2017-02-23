776217384e56fb2b831be33f312e22db92bba611
clturner <clturner23@gmail.com>
2017-02-23 22:56:17 +0000

    concatonate two strings with src to n bytes

---
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * _strncat - concatenates two strings second string to n bytes
+ * @dest: destination file to be appended to
+ * @src: file to append to dest
+ * @n: append n bytes of src
+ * Description: function concatenates two strings 2nd to n bytes
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+  int i = 0;
+  int j = 0;
+
+  while (dest[i] != '\0')
+    {
+      i++;
+    }
+  while (src[j] != '\0' && j < n)
+    {
+      dest[i] = src[j];
+      i++;
+      j++;
+    }
+  return (dest);
+}
+
+
+
