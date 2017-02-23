978cfab42038057889692ecc2bbbad18a50e13e5
Chris Turner <clturner23@gmail.com>
2017-02-23 23:24:45 +0000

    concatonate

--- 0x05-pointers_arrays_strings/1-strncat.c
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -9,21 +9,18 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-  int i = 0;
-  int j = 0;
+	int i = 0;
+	int j = 0;
 
-  while (dest[i] != '\0')
-    {
-      i++;
-    }
-  while (src[j] != '\0' && j < n)
-    {
-      dest[i] = src[j];
-      i++;
-      j++;
-    }
-  return (dest);
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	return (dest);
 }
-
-
-
