c163d727066ac78abd7c8111e6c774098571f30f
Chris Turner <clturner23@gmail.com>
2017-02-24 04:46:51 +0000

    remove whitespace

--- 0x05-pointers_arrays_strings/4-rev_array.c
+++ 0x05-pointers_arrays_strings/4-rev_array.c
@@ -1,9 +1,10 @@
 #include "holberton.h"
 /**
- *
- *
- *
- *
+ * reverse_array - function that reverses the content of an array of integers.
+ * @a: pointer to array
+ * @b: number of array elements
+ * Description: function that reverses the content of an array of integers
+ * Return: void
  */
 void reverse_array(int *a, int n)
 {
@@ -11,11 +12,10 @@ void reverse_array(int *a, int n)
 	int t;
 	int end = n - 1;
 
-	for (c = 0; c < n / 2; c++)
+	for (c = 0; c < n / 2; c++, end--)
 	{
 		t = a[c];
 		a[c] = a[end];
 		a[end] = t;
-		end--;
- 	}
+	}
 }
