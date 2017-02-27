eb3150c8746b310d329b1e8d703ba5c1eb99c925
clturner <clturner23@gmail.com>
2017-02-27 23:43:26 +0000

    Returns a pointer to the first occurrence of the character c in the string s

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -3,20 +3,24 @@
  * _strchr - Returns a pointer to first occurrence of character c in string s
  * @s: Returns a pointer to the first occurrence
  * @c: charchter c to look for
- * Description: Returns a pointer to first occurrence of character c in string s
- * Return: pointer s
+ * Description: Returns pointer to first character c in string s
+ * Return: pointer s or NULL
  *
  */
 char *_strchr(char *s, char c)
 {
 	unsigned int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
 		{
 			return (&s[i]);
- 		}
+		}
+		else if (s[i] == '\0')
+		{
+			return ("NULL");
+		}
 	}
-	return (s);
+	return (0);
 }
