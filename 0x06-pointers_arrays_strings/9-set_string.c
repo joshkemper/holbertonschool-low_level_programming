b9b46ad762d6e7eb0b79e6aadc4dd262e2ead258
Chris Turner <clturner23@gmail.com>
2017-03-01 04:25:22 +0000

    remove whitespace

--- 0x06-pointers_arrays_strings/9-set_string.c
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 
 /**
- * set_string - 
+ * set_string - sets the value of a pointer to a char
  * @s: double pointer to astring
  * @to: pointer to a string
  * Return: void
  */
 void set_string(char **s, char *to)
 {
-  *s = to;
+	*s = to;
 }
