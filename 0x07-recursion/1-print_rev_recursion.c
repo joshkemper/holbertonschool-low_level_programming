e5bbd8a4f3462993c22127822f9c481d93d3e4b2
Chris Turner <clturner23@gmail.com>
2017-03-02 01:55:17 +0000

    print string in reverse

--- 0x07-recursion/1-print_rev_recursion.c
+++ 0x07-recursion/1-print_rev_recursion.c
@@ -1,19 +1,17 @@
 #include "holberton.h"
 /**
- * _print_rev_recursion - function that prints a string in reverse
- * @s: pointer to a string
- * Description: prints a string in reverse
- * Return: void
+ ** _print_rev_recursion - prints a string in reverse
+ ** @s: pointer to string
+ **
+ ** Return: void
  */
+
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
 		return;
 	}
-	else
-	{
 	_print_rev_recursion(s + 1);
 	_putchar(*s);
-	}
 }
