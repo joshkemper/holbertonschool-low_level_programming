df18ac2357fd6d9ddb5867c4061885bf3b50ea60
Chris Turner <clturner23@gmail.com>
2017-03-04 01:04:12 +0000

    program that prints its name, followed by a new line

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - print name of program
+ *
+ *
+ * Return: 0 success
+ */
+int main (int argc, char* argv[])
+{
+	int i;
+
+	for (i = 0;i < argc; i++)
+	{
+		printf("%s", argv[i]);
+	}
+	putchar('\n');
+	return (0);
+}
