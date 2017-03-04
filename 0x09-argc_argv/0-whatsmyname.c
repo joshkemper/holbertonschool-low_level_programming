603dd574508ce420a06c13101e64c5a167d7342d
Chris Turner <clturner23@gmail.com>
2017-03-04 23:49:09 +0000

    program that prints its name, followed by a new line.

--- 0x09-argc_argv/0-whatsmyname.c
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -6,11 +6,11 @@
  *
  * Return: 0 success
  */
-int main (int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0;i < argc; i++)
+	for (i = 0; i < argc; i++)
 	{
 		printf("%s", argv[i]);
 	}
