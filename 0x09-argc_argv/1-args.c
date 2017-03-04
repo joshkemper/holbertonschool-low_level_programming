6e4e13ad88814266db6e389ad11988caa8175689
Chris Turner <clturner23@gmail.com>
2017-03-04 01:17:42 +0000

    program that prints the number of arguments passed into it.

---
+++ 0x09-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * Main - entry point
+ *
+ *
+ * Return: 0 for success
+ */
+int main (int argc, char* argv)
+{
+	printf("%d", argc - 1);
+	putchar('\n');
+	return (0);
+}
