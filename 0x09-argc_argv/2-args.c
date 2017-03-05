3dfe8d0fc8803fa85d29bad8da6d7637c88af9da
Chris Turner <clturner23@gmail.com>
2017-03-05 01:23:12 +0000

    add *

--- 0x09-argc_argv/2-args.c
+++ 0x09-argc_argv/2-args.c
@@ -1,4 +1,3 @@
-#include "holberton.h"
 #include <stdio.h>
 /**
  * main - entry point
@@ -7,7 +6,7 @@
  *
  * Return: 0 success
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 	int i;
 
