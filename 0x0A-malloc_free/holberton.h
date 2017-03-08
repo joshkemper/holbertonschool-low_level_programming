ec523176281ac5b10b7bcb6461ebfae7a070a1da
Chris Turner <clturner23@gmail.com>
2017-03-08 04:07:49 +0000

    add concatonate string function

--- 0x0A-malloc_free/holberton.h
+++ 0x0A-malloc_free/holberton.h
@@ -21,4 +21,4 @@ char *_strstr(char *haystack, char *needle);
 int myAtoi(char *str);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
-
+char *str_concat(char *s1, char *s2);
