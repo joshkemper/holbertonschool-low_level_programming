a706c49cbb6494c580683e7dff070422eed6b76d
Chris Turner <clturner23@gmail.com>
2017-03-08 00:23:57 +0000

    add helper function _strlen

--- 0x0A-malloc_free/1-strdup.c
+++ 0x0A-malloc_free/1-strdup.c
@@ -1,5 +1,24 @@
-#include <holberton.h>
-#include <stdlib.h>s
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * _strlen - counts the length of a given string
+ * @s: pointer to a string , characters in string are counted
+ * Description: counts the length of string pointed to by *s
+ *
+ * Return: count of charachters in a string
+ */
+int _strlen(char *s)
+{
+	int count;
+
+	count = 0;
+	while (*s != 0)
+	{
+		count++;
+		s++;
+	}
+	return (count);
+}
 /**
  * _strdup - returns a pointer to a newly allocated space in memory,
  * @str: pointer to a string
@@ -9,7 +28,7 @@
 char *_strdup(char *str)
 {
 	char *s;
-	unsigned int i = 0;
+	int i = 0;
 
 	if (str == '\0')
 	{
