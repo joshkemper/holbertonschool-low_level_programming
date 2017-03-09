a348986c9215067508b1de5c74370139ebf819b3
Chris Turner <clturner23@gmail.com>
2017-03-09 21:21:14 +0000

    add limits.h

--- 0x0B-more_malloc_free/0-malloc_checked.c
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -1,9 +1,11 @@
 #include "holberton.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
 /**
  * malloc_checked - function that allocates memory using malloc
- * @b: byten needed to allocate
- * Return: pointer ptr
+ * @b: bytes needed to allocate
+ * Return: pointer ptr / return 98 for fail
  */
 void *malloc_checked(unsigned int b)
 {
