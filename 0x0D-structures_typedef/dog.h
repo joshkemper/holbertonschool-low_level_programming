48a6f7b9a7dc3c1542331d632159de13a8704278
Chris Turner <clturner23@gmail.com>
2017-03-13 23:50:20 +0000

    dog.h

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,5 +1,6 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef DOG_H
+#define DOG_H
+
 /**
  * struct dog - Dog struct
  * @name: name of dog
@@ -7,10 +8,12 @@
  * @owner: owners name
  * Description: struct for dog info
  */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
