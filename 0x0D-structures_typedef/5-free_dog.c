ab32c0597d00ceea275168d75c900bcc330bcfd6
Chris Turner <clturner23@gmail.com>
2017-03-14 23:38:20 +0000

    function that frees dogs

--- 0x0D-structures_typedef/5-free_dog.c
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include "dog.h"
-
+#include <stdlib.h>
 /**
- * free_dog -  function that frees dogs
+ * free_dog - function that frees dogs
  * @d: pointer to dog instance
- * return: void
+ * Return: void
  */
 
-void free_dog(dog_t *d);
+void free_dog(dog_t *d)
 {
 	free(d->name);
 	free(d->owner);
