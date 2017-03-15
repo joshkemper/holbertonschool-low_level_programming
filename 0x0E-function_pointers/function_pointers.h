a13df4cc34615ac6b84b18a7d38f5eb37a95775a
Chris Turner <clturner23@gmail.com>
2017-03-15 20:12:18 +0000

    add array_iterator

--- 0x0E-function_pointers/function_pointers.h
+++ 0x0E-function_pointers/function_pointers.h
@@ -1,6 +1,10 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+#include <stddef.h>
 void print_name(char *name, void (*f)(char *));
 void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
+void array_iterator(int *array, size_t size, void (*action)(int));
+void print_elem(int elem);
+void print_elem_hex(int elem);
 #endif
