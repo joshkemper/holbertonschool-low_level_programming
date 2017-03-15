e73b52352b41dc42d4332e9caafc039e4ce4b166
Chris Turner <clturner23@gmail.com>
2017-03-15 21:08:57 +0000

    add init_index function

--- 0x0E-function_pointers/function_pointers.h
+++ 0x0E-function_pointers/function_pointers.h
@@ -7,4 +7,5 @@ void print_name_uppercase(char *name);
 void array_iterator(int *array, size_t size, void (*action)(int));
 void print_elem(int elem);
 void print_elem_hex(int elem);
+int int_index(int *array, int size, int (*cmp)(int));
 #endif
