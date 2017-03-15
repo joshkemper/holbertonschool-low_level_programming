f350a8718b5e8a0f803b46d7da1696e8f1ccf3bb
Chris Turner <clturner23@gmail.com>
2017-03-15 17:21:16 +0000

    add if NULL case for name variable

--- 0x0E-function_pointers/0-print_name.c
+++ 0x0E-function_pointers/0-print_name.c
@@ -1,7 +1,19 @@
 #include "function_pointers.h"
 #include <stdlib.h>
+/**
+ * print_name - Write a function that prints a name
+ * @name:
+ *
+ */
 
 void print_name(char *name, void (*f)(char *))
 {
-	(*f)(name);
+	if (name == NULL)
+	{
+		return;
+	}
+	else
+	{
+		(*f)(name);
+	}
 }
