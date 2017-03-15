9f0b364cb99f6be0e19ade87aee14b8cdc42c044
Chris Turner <clturner23@gmail.com>
2017-03-15 18:09:42 +0000

    add check to see if functipn is null

--- 0x0E-function_pointers/0-print_name.c
+++ 0x0E-function_pointers/0-print_name.c
@@ -1,5 +1,7 @@
 #include "function_pointers.h"
 #include <stdlib.h>
+#include <stdio.h>
+
 /**
  * print_name - Write a function that prints a name
  * @name: pointer to a string called name
@@ -9,7 +11,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL)
+	if (name == NULL || *f == NULL)
 	{
 		return;
 	}
