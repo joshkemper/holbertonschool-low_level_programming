196e6a44176d6d7ff5b299128e988ed167b59a4e
Chris Turner <clturner23@gmail.com>
2017-03-17 17:27:53 +0000

    function that returns the sum of a variable list of parameters

---
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * sum_them_all - returns the summ of a vriable size list of arguments
+ * @n: number of variable arguments listed by caller
+ * Return: sum of varible size list of arguments
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list argumentList;
+	int sum = 0;
+	unsigned int x;
+
+	va_start(argumentList, n);
+	for (x = 0; x < n; x++)
+	{
+		sum = sum + va_arg(argumentList, int);
+	}
+	va_end(argumentList);
+	return (sum);
+}
