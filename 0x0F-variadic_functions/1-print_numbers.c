700c3109c240fa7d367b547a13829dd55225e3e1
Chris Turner <clturner23@gmail.com>
2017-03-18 00:25:41 +0000

    remove one printf argument and condense if statements into one statement

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -14,14 +14,20 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int x;
 
 	va_start(argumentList, n);
-	if (separator == NULL)
+
 	{
-		separator = "";
+		for (x = 0; x < n; x++)
+		{
+			if (separator != NULL && x != n - 1)
+			{
+				printf("%d%s",va_arg(argumentList, int), separator);
+			}
+			else
+			{
+			printf("%d",va_arg(argumentList, int));
+			}
+		}
 	}
-	for (x = 0; x < n - 1 ; x++)
-	{
-		printf("%d%s",va_arg(argumentList, int), separator);
-	}
-	printf("%d\n",va_arg(argumentList, int));
+	printf("\n");
 	va_end(argumentList);
 }
