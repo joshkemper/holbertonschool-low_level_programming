351bc1ec63f96183180bff017a5c64ef5c20fff6
Chris Turner <clturner23@gmail.com>
2017-03-20 04:41:34 +0000

    add pointer to argument lis of strings

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -13,21 +13,23 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list argumentList;
 	unsigned int x;
+	char *ptr;
 
 	va_start(argumentList, n);
 
 	for (x = 0; x < n; x++)
 	{
-		if (va_arg(argumentList, char *) == NULL)
+		ptr = va_arg(argumentList, char*);
+		if (ptr == NULL)
 		{
 			printf("(nil)");
 		}
 		if (x != n - 1  && separator != NULL)
 		{
-			printf("%s", va_arg(argumentList, char *));
+			printf("%s%s", ptr, separator);
 		}
-		printf("%s%s", va_arg(argumentList, char *), separator);
 	}
+	printf("%s", ptr);
 	va_end(argumentList);
 	printf("\n");
 }
