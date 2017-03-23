9754cf23195f3463786d7b0101c894a70696a2ed
Chris Turner <clturner23@gmail.com>
2017-03-23 21:27:05 +0000

    function to print all elemants in a linked list, header file and README file

---
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+#include <unistd.h>
+
+/**
+ * print_list - print elements of struct
+ * @h: head of structure
+ * Return: nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t nodes;
+	int next;
+
+	nodes = 0;
+	while (h != NULL)
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		nodes++;
+	}
+	return(nodes);
+}
---
+++ 0x11-singly_linked_lists/README.md
@@ -0,0 +1,4 @@
+Functions for creating , traversing, deleting and manipulating linked lists.
+
+0-print_list.c - function that prints all the elements of a linked list
+prototype: size_t print_list(const list_t *h);
