faa96a2e8e79012f491e0705946aad991e2b87df
Chris Turner <clturner23@gmail.com>
2017-03-24 00:36:39 +0000

    add strln_recursion function

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -4,6 +4,26 @@
 #include "lists.h"
 
 /**
+ * _strlen_recursion - function that returns the length of a string
+ * @s: pointer to a string
+ * Description: returns the length of a string
+ * Return: 0 for no string, i + 1 (length of string) for success
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		i = (_strlen_recursion(s + 1));
+		return (i + 1);
+	}
+}
+/**
  * add_node - function that adds a new node at the beginning of a linked list
  * @head: pointer to pointer to begining of linked list
  * @str: string to copy into new node
@@ -13,7 +33,7 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *newNode;
 	char *new_str = strdup(str);
-	int newLen = strlen(new_str);
+	int newLen = _strlen_recursion(new_str);
 
 	newNode = malloc(sizeof(list_t));
 	if (newNode == NULL)
