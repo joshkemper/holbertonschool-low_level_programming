618b501d475783b075be587e2cfae9e08458f053
Chris Turner <clturner23@gmail.com>
2017-03-28 01:36:37 +0000

    function to free a list

---
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+#include <unistd.h>
+
+/**
+ * free_listint2 - function that free a linked list
+ * @head: double pointer to head of linked list
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp_node;
+
+	while (*head != NULL)
+	{
+		temp_node = *head;
+		*head = temp_node->next;
+		free(temp_node);
+	}
+}
