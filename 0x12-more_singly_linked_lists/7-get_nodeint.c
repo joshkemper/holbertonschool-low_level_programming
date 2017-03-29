806a07f357b9f36c556880d9502fd269f84dc5f3
Chris Turner <clturner23@gmail.com>
2017-03-29 01:25:31 +0000

    add return to comments

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -8,6 +8,7 @@
  * get_nodeint_at_index - returns the nth node of a linked list
  * @head: pointer to head of a linked list
  * @index: index of node to return
+ * Return: nth (index) node of linked list
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
