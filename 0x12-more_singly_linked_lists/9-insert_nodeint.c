61816464aea50f4f1d91e414b4e7386103267db7
Chris Turner <clturner23@gmail.com>
2017-03-29 21:12:27 +0000

    add if temp = NULL case

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,27 +8,43 @@
  * @head: double pointer to head of linked list
  * @idx: index of list to stop at
  * @n: value of (n) for newly inserted node
+ * Return: new_node
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node;
-	listint_t *temp_node;
+	listint_t *temp;
 	unsigned int count = 0;
 
-	temp_node = *head;
-	while (count != idx - 1)
-	{
-		temp_node = temp_node->next;
-		count++;
-	}
+	temp = *head;
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
 	new_node->n = n;
-	new_node->next = temp_node->next;
-	temp_node->next = new_node;
-	return (new_node);
+	if (idx == 0)
+	{
+		new_node->next = temp;
+		*head = new_node;
+		return (new_node);
+	}
+	while (temp != NULL)
+	{
+		while (count != idx - 1)
+		{
+			temp = temp->next;
+			count++;
+			if (temp == NULL)
+			{
+				free(new_node);
+				return (NULL);
+			}
+		}
+		new_node->next = temp->next;
+		temp->next = new_node;
+		return (new_node);
+	}
+	return (NULL);
 }
