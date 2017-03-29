12c3e83d6ea2545f0d236b29d729c03baf8b4da9
Chris Turner <clturner23@gmail.com>
2017-03-29 00:59:02 +0000

    add if head is null case

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -14,10 +14,16 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp_node;
 
+	if ( *head == NULL)
+	{
+		return;
+	}
 	while (*head != NULL)
 	{
 		temp_node = *head;
 		*head = temp_node->next;
 		free(temp_node);
 	}
+	*head = NULL;
+
 }
