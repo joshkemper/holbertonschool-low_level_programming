049e5df040c67c13d0e54a1135682dca57fd01d9
Chris Turner <clturner23@gmail.com>
2017-03-31 18:13:18 +0000

    add if NULL case

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -14,6 +14,10 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int flip = 1;
 	int i;
 
+	if (b == NULL)
+	{
+		return (0);
+	}
 	for (i = 0; b[i] != '\0'; b++)
 	{
 		if (b[i] != '0' && b[i] != '1')
