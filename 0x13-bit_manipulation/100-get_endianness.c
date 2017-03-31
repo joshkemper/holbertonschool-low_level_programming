53f336ab7ef1d9a36a98a7abe952d61cd10780a0
Chris Turner <clturner23@gmail.com>
2017-03-31 21:34:48 +0000

    function that checks the endianness

---
+++ 0x13-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * get_endianness -  checks the endianness
+ * Return: void
+ *
+ */
+
+int get_endianness(void)
+{
+	unsigned int x;
+	char *c = (char *) &x;
+
+	if (*c)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
