24492158822cfbc5ee376d39e30ce2b709c66007
Chris Turner <clturner23@gmail.com>
2017-03-31 18:16:13 +0000

    remove spae in define statement

--- 0x13-bit_manipulation/holberton.h
+++ 0x13-bit_manipulation/holberton.h
@@ -1,6 +1,6 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-
+#include <unistd.h>
 unsigned int binary_to_uint(const char *b);
 int _pow_recursion(int x, int y);
 void print_binary(unsigned long int n);
