0d78a0ba38bf94e093c8f6c1ab47a8dc07420e66
Chris Turner <clturner23@gmail.com>
2017-04-04 21:57:28 +0000

    move malloc block to top

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -11,14 +11,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd;
 	int fdread, fdwrite, fdclose;
 
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		return (-1);
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL)
-		return (-1);
 	fdread = read(fd, buf, letters);
 	if (fdread == -1)
 		return (0);
