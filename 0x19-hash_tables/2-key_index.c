280057616d6c6ce003b3486351e41be2d51b65de
clturner <clturner23@gmail.com>
2017-05-31 22:54:04 +0000

    gets the index for a given key

---
+++ 0x19-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Returns the index at which the key/value pair should be stored
+ * @key: key
+ * @size: size of hashtable
+ * Return: the index key belongs to
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	int c;
+
+	c = hash_djb2(key) % size;
+	return (c);
+}
