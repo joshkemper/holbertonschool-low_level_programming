2c5a330a199bbf9566946546efc6fe5d8c52f1b6
clturner <clturner23@gmail.com>
2017-05-31 22:15:17 +0000

    add parenthesis

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -9,24 +9,24 @@
  * Return: pointer to table
  */
 
-hash_table_t * hash_table_create(unsigned long int size)
+hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hashtable;
 
 	if (size == 0)
 	{
-		return NULL;
+		return (NULL);
 	}
 	hashtable = malloc(sizeof(hash_table_t));
 	if (hashtable == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 	hashtable->size = size;
 	hashtable->array = calloc(size, sizeof(hash_node_t *));
 	if (hashtable->array == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
-        return hashtable;
+	return (hashtable);
 }
