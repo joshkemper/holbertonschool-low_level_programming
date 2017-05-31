86c444650b32d18058251a135b07f7a72f900164
clturner <clturner23@gmail.com>
2017-05-31 22:03:38 +0000

    change back to malloc

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -19,7 +19,7 @@ hash_table_t * hash_table_create(unsigned long int size)
 		return NULL;
 	}
 
-	hashtable->array = calloc(size, sizeof(hash_node_t *));
+	hashtable->array = malloc(size, sizeof(hash_node_t *));
 	if (hashtable->array == NULL)
 	{
 		free(hashtable);
