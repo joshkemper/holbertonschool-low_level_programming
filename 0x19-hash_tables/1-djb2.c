f7946d684b433e32fdf30c02e741c6749c0e5bdb
clturner <clturner23@gmail.com>
2017-05-31 21:58:22 +0000

    add documnetation

--- 0x19-hash_tables/1-djb2.c
+++ 0x19-hash_tables/1-djb2.c
@@ -1,5 +1,10 @@
 #include "hash_tables.h"
 
+/**
+ * hash_djb2 - create a hash value for a key
+ * @str: key to use
+ * Return: hash value
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
