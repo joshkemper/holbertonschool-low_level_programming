ac43d1e444c45851693ac22a039f7bfa67801a89
clturner <clturner23@gmail.com>
2017-05-31 21:42:03 +0000

    change to size of hash node t

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -18,7 +18,7 @@ hash_table_t * hash_table_create(unsigned long int size)
 		return NULL;
 	}
 
-	hashtable->array = malloc(sizeof(char *) * size);
+	hashtable->array = malloc(sizeof(hash_node_t *) * size);
 	if (hashtable->array == NULL)
 	{
 		free(hashtable->array);
