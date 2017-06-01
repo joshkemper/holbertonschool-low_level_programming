cd9d794bfcee4bed302a174aa280b8c30bc59f20
clturner <clturner23@gmail.com>
2017-06-01 01:20:52 +0000

    add if cases for linked list already exsist

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -14,14 +14,42 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *newNode;
 
 	d = hash_djb2((unsigned char *)key) % ht->size;
+	printf("node %p:\n", (void *)ht->array[d]);
 	newNode = malloc(sizeof(hash_node_t));
 	if (newNode == NULL)
 	{
 		return (0);
 	}
-	newNode->key = strdup(key);
-	newNode->value = strdup(value);
-	newNode->next = NULL;
-	ht->array[d] = newNode;
+	if (ht->array[d] != NULL)
+	{
+		while (ht->array[d]->key != NULL)
+		{
+			if (strcmp(ht->array[d]->key, key) != 0)
+			{
+				printf("array key %s: reg key %s\n", ht->array[d]->key, key);
+				ht->array[d] = ht->array[d]->next;
+				d++;
+			}
+			else
+			{
+				printf("2nd array key %s: 2nd reg key %s\n", ht->array[d]->key, key);
+				newNode->key = strdup(key);
+				newNode->value = strdup(value);
+				newNode->next = NULL;
+				ht->array[d] = newNode;
+				printf("2nd node %p:\n", (void *)ht->array[d]);
+				break;
+			}
+		}
+	}
+	else
+	{
+		newNode->key = strdup(key);
+		printf("key %s:\n", newNode->key);
+		newNode->value = strdup(value);
+		newNode->next = NULL;
+		ht->array[d] = newNode;
+		printf("3rd node %p:\n", (void *)ht->array[d]);
+	}
 	return (1);
 }
