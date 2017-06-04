0ca2c26ad6417980f9475bd1933055b5222b606b
clturner <clturner23@gmail.com>
2017-06-04 23:07:30 +0000

    get a value for key from hash table

---
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - get a value for a key in hash table
+ * @ht: hash table
+ * @key: key to match and return coresponding value
+ * Return: value or NULL if no key matches
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+
+	unsigned int index;
+	hash_node_t *tempNode = NULL;
+	index = hash_djb2((unsigned char *)key) % ht->size;
+	if (ht->array[index])
+	{
+		tempNode = ht->array[index];
+		while (tempNode != NULL)
+		{
+			if (strcmp(tempNode->key, key) == 0)
+			{
+				return (tempNode->value);
+			}
+			tempNode = tempNode->next;
+		}
+	}
+	return (NULL);
+}
