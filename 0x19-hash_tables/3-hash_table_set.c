3742c89824990a7f0cd25466298282d5959b4a8f
clturner <clturner23@gmail.com>
2017-06-04 22:10:46 +0000

    add last esle section back in

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -40,7 +40,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
                 newNode->next = ht->array[index];
                 ht->array[index] = newNode;
 	}
-/*
 	else
 	{
 		newNode->key = strdup(key);
@@ -48,6 +47,5 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		newNode->next = ht->array[index];
 		ht->array[index] = newNode;
 	}
-*/
 	return (1);
 }
