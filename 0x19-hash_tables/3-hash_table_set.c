1bd8ce704f80b7729da6282e4cb96bd57a24cd04
clturner <clturner23@gmail.com>
2017-06-04 21:28:37 +0000

    revert back to old version

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -13,34 +13,32 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *newNode, *tempNode;
 
-
+	printf("start\n");
 	if (key == NULL || value == NULL || ht == NULL)
 	{
 		return (0);
 	}
 	index = hash_djb2((unsigned char *)key) % ht->size;
 	newNode = malloc(sizeof(hash_node_t));
+	printf("got into beginiing\n");
 	if (newNode == NULL)
 	{
 		return (0);
 	}
-	if (ht->array[index] != NULL)
+	printf("got to if case\n");
+	if (ht->array[index])
 	{
+		printf("got up in here\n");
 		if (strcmp(ht->array[index]->key, key) == 0)
 		{
+			printf("got here\n");
 			ht->array[index]->value = strdup(value);
 		}
-		if (ht->array[index]->next != NULL)
-		{
 			ht->array[index] = ht->array[index]->next;
-		}
-		else
-		{
-			newNode->key = strdup(key);
-			newNode->value = strdup(value);
-			newNode->next = ht->array[index];
-			ht->array[index] = newNode;
-		}
 	}
+	newNode->key = strdup(key);
+	newNode->value = strdup(value);
+	newNode->next = ht->array[index];
+	ht->array[index] = newNode;
 	return (1);
 }
