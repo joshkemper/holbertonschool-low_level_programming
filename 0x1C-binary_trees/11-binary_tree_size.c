e1ef12a23afd161e1df5ca6757fe816347403188
clturner <clturner23@gmail.com>
2017-07-23 19:07:05 +0000

    revert back to older appraoach to solve

--- 0x1C-binary_trees/11-binary_tree_size.c
+++ 0x1C-binary_trees/11-binary_tree_size.c
@@ -10,15 +10,17 @@
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	int count;
-
-	count = 0;
 	if (tree == NULL)
         {
                 return (0);
         }
-	count = count + 1;
-	binary_tree_size(tree->left);
-	binary_tree_size(tree->right);
-	return(count);
+      
+
+}
+
+size_t binary_tree_size_helper(const binary_tree_t *tree, int counter)
+{
+
+
+
 }
