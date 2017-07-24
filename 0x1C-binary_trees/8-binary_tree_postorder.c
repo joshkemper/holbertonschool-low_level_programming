6db469f44ec89cfe060c7c2e61a51fda95e01cef
clturner <clturner23@gmail.com>
2017-07-24 22:27:29 +0000

    add comments and remove some spaces

---
+++ 0x1C-binary_trees/8-binary_tree_postorder.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - prints a binary tree using pre-order traversal
+ * @tree: pointer to binary tree
+ * @func: holds ->n value of a node
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL)
+	{
+		return;
+	}
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
+
