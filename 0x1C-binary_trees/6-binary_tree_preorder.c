75e6e466ba5b62fe25fb837fc3d2648813a3c1cb
clturner <clturner23@gmail.com>
2017-07-24 22:12:59 +0000

    add preorder print function

--- 0x1C-binary_trees/6-binary_tree_preorder.c
+++ 0x1C-binary_trees/6-binary_tree_preorder.c
@@ -10,12 +10,11 @@
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	func = func;
 	if (tree == NULL)
 	{
 		return;
-	}
-	printf("%d\n", tree->n);
+	};
+	func(tree->n);
 	binary_tree_preorder(tree->left, func);
 	binary_tree_preorder(tree->right, func);
 }
