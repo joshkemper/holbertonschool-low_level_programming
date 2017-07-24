4b15e46b2e1a80a4369f02055506e4d22a31ed76
clturner <clturner23@gmail.com>
2017-07-24 02:10:30 +0000

    add recursive call to tree->right in return

--- 0x1C-binary_trees/11-binary_tree_size.c
+++ 0x1C-binary_trees/11-binary_tree_size.c
@@ -26,5 +26,5 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	{
 		return (2);
 	}
-	return (1 + binary_tree_size(tree->left));
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
