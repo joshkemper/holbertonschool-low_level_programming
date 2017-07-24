6576caa066c5c8d81fc5a5e16cc94f78ea6661f9
clturner <clturner23@gmail.com>
2017-07-24 20:42:18 +0000

    measures the balance factor of a binary tree

--- 0x1C-binary_trees/14-binary_tree_balance.c
+++ 0x1C-binary_trees/14-binary_tree_balance.c
@@ -2,30 +2,35 @@
 #include <stdio.h>
 #include "binary_trees.h"
 
+/**
+ * binary_tree_balance - measures the balance factor of a tree
+ * @tree: pointer to a tree
+ * Return: balance factor
+ */
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	int count;
 	int count1;
-        const binary_tree_t *temp;
+	const binary_tree_t *temp;
 	const binary_tree_t *temp1;
 
 	temp1 = tree;
-        temp = tree;
+	temp = tree;
 	count1 = 0;
-        count = 0;
+	count = 0;
 	if (tree == NULL)
 	{
 		return (0);
 	}
-        while (temp->left != NULL)
-        {
-                temp = temp->left;
-                count += 1;
-        }
+	while (temp->left != NULL)
+	{
+		temp = temp->left;
+		count += 1;
+	}
 	while (temp1->right != NULL)
 	{
 		temp1 = temp1->right;
 		count1++;
 	}
-        return (count - count1);
+	return (count - count1);
 }
