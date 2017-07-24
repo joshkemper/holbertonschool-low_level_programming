fb1f50aa81b9aa35b052080cee15f152bc2fb704
clturner <clturner23@gmail.com>
2017-07-24 20:47:30 +0000

    remove whitespace

--- 0x1C-binary_trees/15-binary_tree_is_full.c
+++ 0x1C-binary_trees/15-binary_tree_is_full.c
@@ -2,69 +2,24 @@
 #include <stdio.h>
 #include "binary_trees.h"
 
+/**
+ * binary_tree_is_full - checks if a binary tree is full
+ * @tree: pointer to a tree
+ * Return: 0 if not ful 1 if full
+ */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	const binary_tree_t *temp;
-        const binary_tree_t *temp1;
-	int left_score = 0;
-	int right_score = 0;
-
-	temp1 = tree;
-        temp = tree;
 	if (tree == NULL)
 	{
-                return (0);
+		return (0);
 	}
-	while (temp->left != NULL)
-        {
-		printf("got in LEFT\n");
-		if (temp->right == NULL)
-		{
-			printf("got LEFT IF 1\n");
-			left_score = 0;
-			break;
-		}
-		if (temp->left == NULL && temp->right != NULL)
-		{
-			printf("got LEFT IF 2\n");
-			left_score = 0;
-			break;
-		}
-		if (temp->left == NULL && temp->right == NULL)
-		{
-
-			printf("got LEFT IF 3\n");
-			left_score = 1;
-			break;
-		}
-                temp = temp->left;
-        }
-        while (temp1->right != NULL)
-        {
-		printf("got in RIGHT\n");
-		if (temp1->left != NULL && temp1->right == NULL)
-                {
-                        right_score = 0;
-			break;
-		}
-		if (temp1->left == NULL && temp1->right != NULL)
-		{
-                        right_score = 0;
-			break;
-                }
-                if (temp1->left == NULL && temp1->right == NULL)
-                {
-		
-                        right_score = 1;
-			break;
-		}
-                temp1 = temp1->right;
-        }
-	printf("left_score %d\n", left_score);
-	if (left_score == 1 && right_score == 1)
+	if (tree->left == NULL && tree->right == NULL)
 	{
-		printf("got in IF RETURN !\n");
 		return (1);
+	}
+	if ((tree->left) && (tree->right))
+	{
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 	}
 	return (0);
 }
