8e520bdd1717cbbc2c44a33da29573489ab5ab5c
clturner <clturner23@gmail.com>
2017-07-24 19:36:18 +0000

    farmatting and betty fix

--- 0x1C-binary_trees/4-binary_tree_is_leaf.c
+++ 0x1C-binary_trees/4-binary_tree_is_leaf.c
@@ -5,6 +5,7 @@
 /**
  * binary_tree_is_leaf- checks if node is a leaf
  * @node: node in tree to check if leaf
+ * Return: 1 if node is a leaf 0 if not a leaf
  */
 
 int binary_tree_is_leaf(const binary_tree_t *node)
@@ -13,13 +14,13 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 	{
 		if (node->left == NULL && node->right == NULL)
 		{
-			return(1);
+			return (1);
 		}
 		else
 		{
-			return(0);
+			return (0);
 		}
 	}
-	return(0);
+	return (0);
 }
 
