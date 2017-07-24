ed1a96ccb62e88cff27babd6f414d57a94622ad2
clturner <clturner23@gmail.com>
2017-07-24 20:24:55 +0000

    remove spaces

--- 0x1C-binary_trees/10-binary_tree_depth.c
+++ 0x1C-binary_trees/10-binary_tree_depth.c
@@ -3,7 +3,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_depth- function that measures the depth of a node in a binary tree
+ * binary_tree_depth - measures the depth of a node in a binary tree
  * @node: pointer to a nade in binary tree
  * Return: count of depth of a givin node
  */
@@ -11,16 +11,16 @@
 size_t binary_tree_depth(const binary_tree_t *node)
 {
 	int count;
-        const binary_tree_t *temp;
+	const binary_tree_t *temp;
 
-        temp = node;
-        count = 0;
-        if (node == NULL)
-                return (0);
-        while (temp->parent != NULL)
-        {
-                temp = temp->parent;
-                count += 1;
-        }
-        return(count);
+	temp = node;
+	count = 0;
+	if (node == NULL)
+		return (0);
+	while (temp->parent != NULL)
+	{
+		temp = temp->parent;
+		count += 1;
+	}
+	return (count);
 }
