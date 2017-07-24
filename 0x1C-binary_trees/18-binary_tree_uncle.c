1d05055029af712297a7dc0e74200944b3f368dc
clturner <clturner23@gmail.com>
2017-07-24 10:45:07 +0000

    function that finds the uncle of a node

---
+++ 0x1C-binary_trees/18-binary_tree_uncle.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+#include <stdbool.h>
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+        if (node->parent == NULL)
+	{
+		return (NULL);
+	}
+        if (node->parent->left == NULL || node->parent->right == NULL)
+		return (NULL);
+	if (node->parent->left != NULL && node->parent->right != NULL)
+	{
+		if (node == node->parent->left)
+		{
+			if (node->parent->parent == NULL)
+			{
+				return (NULL);
+			}
+			return (node->parent->parent->left);
+		}
+		else
+		{
+			if (node->parent->parent == NULL)
+                        {
+				return (NULL);
+			}
+			return (node->parent->parent->right);
+		}
+	}
+        return (NULL);
+}
+
