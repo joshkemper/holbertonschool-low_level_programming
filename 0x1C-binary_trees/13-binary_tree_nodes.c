b470b2019f4f8c29801082600de4f73e92dc4d8d
clturner <clturner23@gmail.com>
2017-07-24 20:36:27 +0000

    remove whitespace

--- 0x1C-binary_trees/13-binary_tree_nodes.c
+++ 0x1C-binary_trees/13-binary_tree_nodes.c
@@ -3,7 +3,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes- function that counts the nodes with at least 1 child in a binary tree
+ * binary_tree_nodes - counts nodes with 1 child
  * @tree: pointer to binary tree
  * Return: number of nodes with at least one child
  */
@@ -11,17 +11,17 @@
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-        {
-                return (0);
-        }
+	{
+		return (0);
+	}
 	if (tree->left == NULL && tree->right == NULL)
-        {
-                return (0);
-        }
+	{
+		return (0);
+	}
 	if (tree->left != NULL && tree->right == NULL)
-        {
-                return (1);
-        }
+	{
+		return (1);
+	}
 	if (tree->left == NULL && tree->right != NULL)
 	{
 		return (1);
