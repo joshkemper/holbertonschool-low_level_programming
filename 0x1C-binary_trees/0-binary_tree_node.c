fe896ffe185b26f64a8e75571834d2c6834d992a
clturner <clturner23@gmail.com>
2017-07-24 15:40:52 +0000

    change order of initiation to match order of struct

--- 0x1C-binary_trees/0-binary_tree_node.c
+++ 0x1C-binary_trees/0-binary_tree_node.c
@@ -2,18 +2,24 @@
 #include <stdlib.h>
 #include "binary_trees.h"
 
-binary_tree_t *binary_tree_node(binary_tree_t* parent, int value)
+/**
+ * binary_tree_node- function that creates a binary tree node
+ * @parent: pointer to root of a binary tree
+ * @value: integer value for newNode
+ * Return: newNode of binary tree
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
+	binary_tree_t *newNode;
+
+	newNode = malloc(sizeof(binary_tree_t));
 	if (newNode == NULL)
 	{
 		return (NULL);
 	}
-
-	newNode->parent = parent;
 	newNode->n = value;
+	newNode->parent = parent;
 	newNode->left = NULL;
 	newNode->right = NULL;
 	return(newNode);
 }
-
