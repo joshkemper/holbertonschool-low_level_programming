244232286d7621b6ccf467e02198c719bb71a5c8
clturner <clturner23@gmail.com>
2017-07-24 21:31:56 +0000

    add comments

--- 0x1C-binary_trees/17-binary_tree_sibling.c
+++ 0x1C-binary_trees/17-binary_tree_sibling.c
@@ -3,6 +3,11 @@
 #include "binary_trees.h"
 #include <stdbool.h>
 
+/**
+ * binary_tree_sibling - finds the sibling of a node
+ * @node: node
+ * Return: sibling of node
+ */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (node->parent == NULL)
