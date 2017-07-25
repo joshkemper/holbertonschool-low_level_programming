6cdb7a41196bbe18d1aee2fef1cfdfbbd1a121f9
clturner <clturner23@gmail.com>
2017-07-25 04:34:49 +0000

    change name of helper function

--- 0x1C-binary_trees/102-binary_tree_is_complete.c
+++ 0x1C-binary_trees/102-binary_tree_is_complete.c
@@ -10,58 +10,60 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-
-        if (tree == NULL)
-        {
-                return (0);
-        }
-        if (tree->left == NULL && tree->right == NULL)
-        {
-                return (1);
-        }
-        if (tree->left != NULL && tree->right == NULL)
-        {
-                return (2);
-        }
-        if (tree->left == NULL && tree->right != NULL)
-        {
-                return (2);
-        }
-        /* Add 1 to count root node */
-        return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
+	if (tree->left != NULL && tree->right == NULL)
+	{
+		return (2);
+	}
+	if (tree->left == NULL && tree->right != NULL)
+	{
+		return (2);
+	}
+/* Add 1 to count root node */
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
 
 /**
- * binary_tree_iscomplete_helper- checks to see if tree is complete
+ * b - checks to see if tree is complete
  * @tree: root of tree
  * @index: index
- * @number_node: number of nodes
+ * @nodes: number of nodes
  * Return: 1 if complete 0 if not complete
  */
-bool binary_tree_iscomplete_helper(const binary_tree_t *tree, unsigned int index, unsigned int number_nodes)
+bool b(const binary_tree_t *tree, unsigned int index, unsigned int nodes)
 {
-        if (tree == NULL)
-                return (true);
-        if (index >= number_nodes)
-                return (false);
-        return (binary_tree_iscomplete_helper(tree->left, 2*index + 1, number_nodes) && binary_tree_iscomplete_helper(tree->right, 2*index + 2, number_nodes));
+	unsigned int i;
+
+	if (tree == NULL)
+		return (true);
+	if (index >= nodes)
+		return (false);
+	i = 2 * index;
+	return (b(tree->left, i + 1, nodes) && b(tree->right, i + 2, nodes));
 }
 
 /**
- * binary_tree_is_complete- function that checks if a binary tree is complete
+ * binary_tree_is_complete - function that checks if a binary tree is complete
  * @tree: root of tree
  * Return: 1 if perfect 0 if not
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-        int c;
-        unsigned int index;
-        unsigned int number_nodes;
+	int c;
+	unsigned int index;
+	unsigned int number_nodes;
 
-        c = 0;
-        index = 0;
-        number_nodes = 0;
-        number_nodes = binary_tree_size(tree);
-        c = binary_tree_iscomplete_helper(tree, index, number_nodes);
-        return (c);
+	c = 0;
+	index = 0;
+	number_nodes = 0;
+	number_nodes = binary_tree_size(tree);
+	c = b(tree, index, number_nodes);
+	return (c);
 }
