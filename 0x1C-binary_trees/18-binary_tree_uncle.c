f50939186a12c6c2632040c6d3c3199d55a3ee1c
clturner <clturner23@gmail.com>
2017-07-25 01:30:40 +0000

    finds the uncle of a node

--- 0x1C-binary_trees/18-binary_tree_uncle.c
+++ 0x1C-binary_trees/18-binary_tree_uncle.c
@@ -10,30 +10,29 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-        if (node->parent == NULL)
+	if (node == NULL)
 	{
 		return (NULL);
 	}
-        if (node->parent->left == NULL || node->parent->right == NULL)
-		return (NULL);
-	if (node->parent->left != NULL && node->parent->right != NULL)
+	if (node->parent == NULL)
 	{
-		if (node == node->parent->left)
-		{
-			if (node->parent->parent == NULL)
-			{
-				return (NULL);
-			}
-			return (node->parent->parent->left);
-		}
-		else
-		{
-			if (node->parent->parent == NULL)
-                        {
-				return (NULL);
-			}
-			return (node->parent->parent->right);
-		}
+		return (NULL);
 	}
-        return (NULL);
+	if (node->parent->parent == NULL)
+	{
+		return (NULL);
+	}
+	if (node == node->parent->parent->left->right)
+	{
+		return (node->parent->parent->right);
+	}
+	else if (node == node->parent->parent->left->left)
+	{
+		return (node->parent->parent->right);
+	}
+	else if (node == node->parent->parent->right->left)
+	{
+		return (node->parent->parent->left);
+	}
+	return (node->parent->parent->left);
 }
