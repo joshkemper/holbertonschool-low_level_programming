79079f2ecc497bc92a7f330ee592a1ccbb95d51e
clturner <clturner23@gmail.com>
2017-07-25 00:53:59 +0000

    make function multi line

--- 0x1C-binary_trees/100-binary_trees_ancestor.c
+++ 0x1C-binary_trees/100-binary_trees_ancestor.c
@@ -10,7 +10,8 @@
  * Return: pointer to common ancestrer node
  */
 
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				     const binary_tree_t *second)
 {
 	const binary_tree_t *temp;
 
