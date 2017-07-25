4415d869a758263740e7b73601335d48aed0598d
clturner <clturner23@gmail.com>
2017-07-25 01:40:19 +0000

    remove if case not needed

--- 0x1C-binary_trees/17-binary_tree_sibling.c
+++ 0x1C-binary_trees/17-binary_tree_sibling.c
@@ -16,12 +16,9 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	}
 	if (node->parent->left == NULL || node->parent->right == NULL)
 		return (NULL);
-	if (node->parent->left != NULL && node->parent->right != NULL)
-	{
-		if (node == node->parent->left)
-			return (node->parent->right);
-		else
-			return (node->parent->left);
-	}
+	if (node == node->parent->left)
+		return (node->parent->right);
+	else
+		return (node->parent->left);
 	return (NULL);
 }
