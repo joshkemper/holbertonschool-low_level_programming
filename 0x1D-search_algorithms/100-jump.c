ff6269cdd6c4eb3c2a264225e2b4644830f91246
clturner <clturner23@gmail.com>
2017-08-16 09:05:19 +0000

    add printing search range lines

--- 0x1D-search_algorithms/100-jump.c
+++ 0x1D-search_algorithms/100-jump.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-/**
- *
- *
- *
- *
- */
 
 int jump_search(int *array, size_t size, int value)
 {
@@ -18,10 +12,10 @@ int jump_search(int *array, size_t size, int value)
 	jump = sqrt(size);
 	left = 0;
 	right = 0;
-
+	i = 0;
 	while (left < size && array[left] <= value)
 	{
-		printf("Value checked array [%lu] = [%d]\n", right, array[right]);
+		printf("Value checked array[%d] = [%d]\n", array[right], array[right]);
 		if ((size - 1) < (left + jump))
 		{
 			right = (size - 1);
@@ -36,6 +30,18 @@ int jump_search(int *array, size_t size, int value)
 		}
 		left += jump;
 	}
+	if (left > right)
+	{
+		printf("Value found between indexes [%lu] and [%lu]\n", right, left);	
+	}
+	else
+	{
+		printf("Value found between indexes [%lu] and [%lu]\n", left, right);
+	}
+	if (array[size-1] < value)
+	{
+		printf("Value checked array[%d] = [%d]\n", array[right], array[right]);
+	}
 	if (left >= size || array[left] > value)
 	{
 		return (-1);
@@ -48,10 +54,14 @@ int jump_search(int *array, size_t size, int value)
 	{
 		right = right;
 	}
-	printf("Value found between indexes [%lu] and [%lu]\n", left, right);
 	for (i = left; i <= right && array[i] <= value; i++)
 	{
-		printf("Value checked array [%lu] = [%d]\n", right, array[i]);
+		if (array[i] == value)
+		{
+			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+			return (i);
+		}
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 	}
 	return (-1);
 }
