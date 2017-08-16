5814de3434b7454523fd73da059792f2c4e76437
clturner <clturner23@gmail.com>
2017-08-16 03:39:24 +0000

    change print functions

--- 0x1D-search_algorithms/1-binary.c
+++ 0x1D-search_algorithms/1-binary.c
@@ -13,52 +13,39 @@ int binary_search(int *array, size_t size, int value)
 	size_t end;
 	size_t mid;
 	size_t i;
-	int flag;
 
-	flag = 0;
+	i = 0;
 	start = 0;
-	end = size;
+	end = size -1;
 	while (start <= end)
 	{
 		printf("Searching in array: ");
-		if (flag == 1)
-		{
-			flag = 0;
-			if ((start - end) >= 2)
-			{
-				printf("%lu, ", mid);
-			}
-			else
-			{
-				printf("%lu", mid);
-			}
-		}
-		for (i = start; i < end ; i++)
+		for (i = start; i < end; i++)
 		{
 			if (i == end-1)
 			{
-				printf("%d", array[i]);
+				printf("%d, ", array[i]);
 			}
 			else
 			{
 				printf("%d, ", array[i]);
 			}
 		}
+		printf("%d", array[i]);
 		printf("\n");
 		mid = ((start + end)/2);
 		if (array[mid] == value)
 		{
-			return(mid);
+			return (mid);
 		}
-		else if (value < array[mid])
+		if (value < array[mid])
 		{
 			end = mid;
 		}
 		else
 		{
-			flag = 1;
-			start = mid+1;
+			start = mid + 1;
 		}
 	}
-	return(-1);
+	return (-1);
 }
