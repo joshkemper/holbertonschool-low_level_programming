c65a0e3dd43dc498a7aa73be1c9392d599c5a379
clturner <clturner23@gmail.com>
2017-08-16 04:17:18 +0000

    print statement indent to far

--- 0x1D-search_algorithms/1-binary.c
+++ 0x1D-search_algorithms/1-binary.c
@@ -24,7 +24,7 @@ int binary_search(int *array, size_t size, int value)
 		printf("Searching in array: ");
 		for (i = start; i < end; i++)
 		{
-				printf("%d, ", array[i]);
+			printf("%d, ", array[i]);
 		}
 		printf("%d", array[i]);
 		printf("\n");
