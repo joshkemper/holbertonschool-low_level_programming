c887a049c22420cac4e03739356674726309a8fd
clturner <clturner23@gmail.com>
2018-03-03 14:56:38 +0000

    change variable name to last_number for clarity

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,23 +9,24 @@
 int main(void)
 {
 	int n;
-	int l;
+        int last_number;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	l = n % 10;
-	if (l == 0)
-	{
-		printf("Last digit of %d is %d and is 0", n, l);
-	}
-	else if (l <= 5)
-	{
-		printf("Last digit of %d is %d and is less than 6 and not 0", n, l);
-	}
-	else if (l >= 6)
-	{
-		printf("Last digit of %d is %d and is greater than 5", n, l);
-	}
-	putchar ('\n');
-	return (0);
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+
+        last_number = n % 10;
+
+        if (last_number > 5)
+        {
+                printf("Last digit of %d is %d and is greater than 5", n, last_number);
+        }
+        else if (last_number == 0)
+        {
+                printf("Last digit of %d is 0 and is 0", n);
+        }
+        else
+        {
+                printf("Last digit of %d is %d and is less than 6 and not 0", n, last_number);
+        }
+        return (0);
 }
