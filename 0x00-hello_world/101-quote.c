0d725b27d062702da4aafca14fdade05427b1ace
clturner <clturner23@gmail.com>
2018-03-03 14:00:12 +0000

    use write function and declare string as buffer

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <stdio.h>
+
 int main(void)
 {
-	if (write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60) != 60) 
-	{
-		write(2, "There was an error writing to standard out\n", 44);
-		return -1;
-	}
-	return 0;
+	int fd;
+	char Buff[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fd = 1;
+	write( fd, Buff, 60);
+	return (1);
 }
